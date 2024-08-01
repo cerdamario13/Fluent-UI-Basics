a00df4591e3e4674e54c899b5f97bee3391fe48f
cerdamario13 <cerdamario13@gmail.com>
2024-08-01 10:36:59 -0500

    button with menu example

---
+++ src/taskpane/components/items/Button_Menu.tsx
@@ -0,0 +1,49 @@
+import { CommandButton, IButtonStyles, IIconProps, Toggle } from "@fluentui/react";
+import * as React from "react";
+
+
+export const Button_Menu = () => {
+
+  return (
+    <>
+      <CommandButton
+        split //Determine the button type if it has a split button or not
+        styles={commandButtonSettings}
+        iconProps={{ iconName: 'Settings' }}
+        menuIconProps={menuSettings}
+        text="Settings"
+        //Button menu
+        menuProps={{
+          items: [
+            { 
+              key: 'emailMessage', text: 'Email message', iconProps: { iconName: 'Mail' }
+            },
+            // Render a custom component
+            {
+              key: 'customRender',
+              text: 'Custom Render',
+              onRender: () => (
+                <Toggle label="Toggle"/>
+              )
+            }
+          ]
+        }}
+      />
+    </>
+  )
+}
+
+const commandButtonSettings: IButtonStyles = {
+  root: { width: 75, height: 35 },
+  splitButtonMenuButton: { border: 'none', background: 'none'}
+};
+const menuSettings: IIconProps = {
+  iconName: "MoreVertical",
+  styles: {
+    root: {
+      cursor: "pointer",
+      fontWeight: "bolder",
+      fontSize: "medium"
+    }
+  }
+}
