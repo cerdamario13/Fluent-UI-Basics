3902304e1f27752239ae0ce82e5fc2d863ba8ad1
cerdamario13 <cerdamario13@gmail.com>
2024-08-01 10:38:57 -0500

    added comments

--- src/taskpane/components/items/Button_Menu.tsx
+++ src/taskpane/components/items/Button_Menu.tsx
@@ -8,13 +8,14 @@ export const Button_Menu = () => {
     <>
       <CommandButton
         split //Determine the button type if it has a split button or not
-        styles={commandButtonSettings}
+        styles={commandButtonSettings} //Style the component additionally
         iconProps={{ iconName: 'Settings' }}
         menuIconProps={menuSettings}
         text="Settings"
         //Button menu
         menuProps={{
           items: [
+            //Render an additional button
             { 
               key: 'emailMessage', text: 'Email message', iconProps: { iconName: 'Mail' }
             },
@@ -33,6 +34,7 @@ export const Button_Menu = () => {
   )
 }
 
+//Additional styles for the component
 const commandButtonSettings: IButtonStyles = {
   root: { width: 75, height: 35 },
   splitButtonMenuButton: { border: 'none', background: 'none'}
