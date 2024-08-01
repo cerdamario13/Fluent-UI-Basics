59dcea24c5097c35b174a6935aa0ed31c605afea
cerdamario13 <cerdamario13@gmail.com>
2024-08-01 09:46:40 -0500

    creating a basic component pages

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -1,46 +1,21 @@
+import { DefaultButton, Stack, Text } from "@fluentui/react";
 import * as React from "react";
-import Header from "./Header";
-import HeroList, { HeroListItem } from "./HeroList";
-import TextInsertion from "./TextInsertion";
-import { makeStyles } from "@fluentui/react-components";
-import { Ribbon24Regular, LockOpen24Regular, DesignIdeas24Regular } from "@fluentui/react-icons";
-import { insertText } from "../taskpane";
 
-interface AppProps {
-  title: string;
-}
+const App: React.FC = () => {
 
-const useStyles = makeStyles({
-  root: {
-    minHeight: "100vh",
-  },
-});
 
-const App: React.FC<AppProps> = (props: AppProps) => {
-  const styles = useStyles();
-  // The list items are static and won't change at runtime,
-  // so this should be an ordinary const, not a part of state.
-  const listItems: HeroListItem[] = [
-    {
-      icon: <Ribbon24Regular />,
-      primaryText: "Achieve more with Office integration",
-    },
-    {
-      icon: <LockOpen24Regular />,
-      primaryText: "Unlock features and functionality",
-    },
-    {
-      icon: <DesignIdeas24Regular />,
-      primaryText: "Create and visualize like a pro",
-    },
-  ];
+  const stackTokens = { childrenGap: 10 };
 
+  
   return (
-    <div className={styles.root}>
-      <Header logo="assets/logo-filled.png" title={props.title} message="Welcome" />
-      <HeroList message="Discover what this add-in can do for you today!" items={listItems} />
-      <TextInsertion insertText={insertText} />
-    </div>
+    <>
+    <Stack tokens={stackTokens}>
+      <Text variant="xLarge">Stack Items</Text>
+      <DefaultButton text="Btn1"/>
+      <DefaultButton text="Btn1"/>
+      <DefaultButton text="Btn1"/>
+    </Stack>
+    </>
   );
 };
 
