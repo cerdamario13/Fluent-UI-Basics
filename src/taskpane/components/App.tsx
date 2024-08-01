bc15188599843fa4e419d31c52dd6086e935f87a
cerdamario13 <cerdamario13@gmail.com>
2024-08-01 10:37:12 -0500

    added a bit more stuff to the home page

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -1,15 +1,20 @@
 import * as React from "react";
+import { Text, Stack } from "@fluentui/react";
 import { Pivot_Comp } from "./items/Pivot_Comp";
 import { Stack_Comp } from "./items/Stack_Comp";
 import { Dropdown_Comp } from "./items/Dropdown_Comp";
+import { Button_Menu } from "./items/Button_Menu";
 
 
 const App: React.FC = () => {
 
   return (
-    <>
-      <Dropdown_Comp />
-    </>
+    <Stack>
+      <Text variant="xLarge">
+        Custom Component
+      </Text>
+      <Button_Menu />
+    </Stack>
   );
 };
 
