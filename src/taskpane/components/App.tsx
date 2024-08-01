cae98aa7e7fc67cae0d53a88fe529adc7e218978
cerdamario13 <cerdamario13@gmail.com>
2024-08-01 10:24:34 -0500

    update app.tsx to render component samples

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import { Pivot_Comp } from "./items/Pivot_Comp";
 import { Stack_Comp } from "./items/Stack_Comp";
+import { Dropdown_Comp } from "./items/Dropdown_Comp";
 
 
 const App: React.FC = () => {
 
   return (
     <>
-      <Pivot_Comp/>
+      <Dropdown_Comp />
     </>
   );
 };
